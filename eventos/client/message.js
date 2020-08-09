2bb076f989f493fdce435c4e82869714df8d4e43
Duarte <descionunesd44@gmail.com>
2020-08-09 19:21:44 -0300

    Update message.js

--- eventos/client/message.js
+++ eventos/client/message.js
@@ -1,11 +1,15 @@
 module.exports = async (client, message ) => {
     
+    const firebase = require('firebase')
+    const database = firebase.database()
+    const { MessageEmbed } = require("discord.js")
+
     client.on("messageUpdate", async (oldMessage, newMessage) => {
     if (oldMessage.content === newMessage.content) {
       return;
     }
   
-    let EditEmbed = new Discord.MessageEmbed()
+    let EditEmbed = new MessageEmbed()
       .setAuthor(`Mensagem Editada`, newMessage.guild.iconURL({dynamic: true}))
       .setColor("#2f3136")
       .setThumbnail(newMessage.author.displayAvatarURL({ dynamic: true }))
@@ -17,5 +21,21 @@ module.exports = async (client, message ) => {
     const canal = newMessage.guild.channels.cache.get(canais)
     if (!canal) return;
     canal.send(EditEmbed);
+  })
+
+  client.on("messageDelete", async (message, channel) => {
+
+    let canais = await database.ref(`Servidores/${message.guild.id}/Canais/CanalLog`).once(`value`)
+    canais = canais.val()
+  
+    let DeleteEmbed = new MessageEmbed()
+      .setAuthor(`Mensagem Deletada`, message.guild.iconURL({dynamic: true}))
+      .setColor("#2f3136")
+      .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+      .setDescription(`Registro de mensagens deletadas por ${message.author}. \n\n **• Informações** \n ▪︎ **Mensagem Deletada**: ${message} \n ▪︎ **No canal**: ${message.channel} \n ▪︎ **Servidor**: ${message.guild.name}`)
+  
+    const canal = message.guild.channels.cache.get(canais);
+      if (!canal) return;
+    canal.send(DeleteEmbed);
   })
 }
