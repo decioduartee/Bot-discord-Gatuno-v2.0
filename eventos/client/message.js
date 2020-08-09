1edb3363715474cd4332d6fbb7b1f36cae580e5a
Duarte <descionunesd44@gmail.com>
2020-08-09 19:57:41 -0300

    update

--- eventos/client/message.js
+++ eventos/client/message.js
@@ -5,6 +5,17 @@ module.exports = async (client, message ) => {
     const firebase = require('firebase')
     const database = firebase.database()
 
+    if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@!${client.user.id}>`)) {
+
+        const embed = new MessageEmbed()
+            .setColor("#2f3136")
+            .setAuthor(`Olá ${message.author.username}`, client.user.displayAvatarURL())
+            .setDescription(`Vi que você me mencionou no chat, se você estiver com duvidas..\nSem problemas estou aqui para te ajudar, se estiver com duvidas\nSobre meus comando Por Favor use **${prefix}ajuda**, qualquer outra duvida\nSobre o Servidor Por Favor entre em contato com um moderador`)
+            .setFooter(`Fui mencionado por ${message.author.username}`,message.author.displayAvatarURL())
+            .setTimestamp();
+        message.channel.send(embed);
+    }
+
     if (message.author.bot || message.system) return null;
     if (!message.content.startsWith(prefix)) return null;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -13,15 +24,4 @@ module.exports = async (client, message ) => {
     if (!cmd) return null;
 
     cmd.run(client, message, args, database);
-
-    if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@!${client.user.id}>`)) {
-
-    const embed = new MessageEmbed()
-        .setColor("#2f3136")
-        .setAuthor(`Olá ${message.author.username}`, client.user.displayAvatarURL())
-        .setDescription(`Vi que você me mencionou no chat, se você estiver com duvidas..\nSem problemas estou aqui para te ajudar, se estiver com duvidas\nSobre meus comando Por Favor use **${prefix}ajuda**, qualquer outra duvida\nSobre o Servidor Por Favor entre em contato com um moderador`)
-        .setFooter(`Fui mencionado por ${message.author.username}`,message.author.displayAvatarURL())
-        .setTimestamp();
-    message.channel.send(embed);
-    }
 }
