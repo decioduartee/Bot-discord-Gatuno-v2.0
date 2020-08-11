6db3ed0e4302774a63705e905ae58dc99d9159f8
Duarte <descionunesd44@gmail.com>
2020-08-11 15:43:26 -0300

    Update np.js

--- comandos/musica/np.js
+++ comandos/musica/np.js
@@ -9,7 +9,10 @@ module.exports = {
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) {
-      return message.channel.send("<:Erro:733785842438635571> **| ERRO AO VER A FILA** \n\n • Não há nada na fila");
+      let embed = new MessageEmbed()
+        .setColor('#2f3136')
+        .setDescription("<:Erro:733785842438635571> **| ERRO AO VER A FILA** \n\n • Não há nada na fila")
+      return message.channel.send(embed);
     }
   
     const song = serverQueue.songs[0];
@@ -17,10 +20,10 @@ module.exports = {
     const left = song.duration - seek;
 
     let nowPlaying = new MessageEmbed()
+      .setColor('#2f3136')
       .setTitle(`<:tocando:733743541066661928> **| Agora está tocando:**`)
       .setDescription(`[**${song.title}**](${song.url}) \n \`\`\`${new Date(seek * 1000).toISOString().substr(11, 8) + " " + createBar((song.duration == 0 ? seek : song.duration), seek, 20)[0] + " " + (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8))}\`\`\``)
       .setThumbnail(song.thumbnail)
-      .setColor('#2f3136')
 
     if (song.duration > 0) nowPlaying.setFooter("Tempo Restante: " + new Date(left * 1000).toISOString().substr(11, 8));
 
