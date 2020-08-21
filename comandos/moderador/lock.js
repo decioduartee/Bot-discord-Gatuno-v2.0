fd203f8df9ed9f36323a9d19c79f349e4416d67c
Duarte <descionunesd44@gmail.com>
2020-08-21 02:28:00 -0300

    Update lock.js

--- comandos/moderador/lock.js
+++ comandos/moderador/lock.js
@@ -5,7 +5,7 @@ module.exports = {
     category: "moderador",
     userPerm: ["MANAGE_MESSAGES"],
     botPerm: ["MANAGE_MESSAGES"],
-    run: async (message, [channel = message.channel]) => {
+    run: async (message, channel) => {
         const type = channel.type === 'text' ? 'SEND_MESSAGES' : 'CONNECT';
         await channel.overwritePermissions(channel.guild.defaultRole, { [type]: false });
         if (message.channel.permissionsFor(message.guild.me).has('SEND_MESSAGES') === false) return true;
