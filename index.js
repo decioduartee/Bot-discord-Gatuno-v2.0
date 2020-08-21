e5123a66f0f585263bce9f6c13ee8654c89b3eb4
Duarte <descionunesd44@gmail.com>
2020-08-21 00:31:58 -0300

    update

--- index.js
+++ index.js
@@ -38,6 +38,38 @@ client.eventos = new Map();
 
 client.on("message", async message => {
 
+  const afk = new db.table("AFKs"), mentioned = await message.mentions.members.first();
+  if (mentioned) {
+  let status = await afk.get(mentioned.id);
+
+    if (status) {
+      const embed = new MessageEmbed()
+        .setColor("#2f3136")
+        .setDescription(`<:certo:736447597102760007> **| MODO AFK ATIVADO** \n\n ${mentioned.user} Está em modo **AFK**\n\n**Lembrete**:\n\`\`\`fix\n${status}\`\`\``)
+        .setThumbnail(mentioned.user.displayAvatarURL({ format: "png", size: 2048, dynamic: true }))
+        .setTimestamp()
+        .setFooter(`Atenciosamente ${message.client.user.username}`, message.client.user.displayAvatarURL());
+      message.channel.send(embed).then(i => i.delete({ timeout: 50000 }));
+    }
+  }
+  
+//_____________________________comando de anti invites___________________
+
+    const blockinvite = await database.ref(`Servidores/${message.guild.id}/Defesa/Blockinvite`).once('value')
+    blockinvite = blockinvite.val()
+
+    if(blockinvite === "on") {
+      let Link = ["discord.gg", "discord.com/invite", "discordapp.com/invite"];
+
+      if (Link.some(word => message.content.toLowerCase().includes(word))) {
+        await message.delete();
+        let embed = new MessageEmbed()
+          .setColor("#2f3136")
+          .setDescription("**Por favor respeite as regas**! não mande convites neste servidor")
+        return message.channel.send(embed).then(m => m.delete({timeout: 10000}))
+      }
+    }
+
   if (message.author.bot && message.guild || message.system) return null;
   if (message.channel.type === "dm") return null;
     
@@ -152,8 +184,6 @@ client.on("message", async message => {
   
   //______________________________________________________________________
   
-
-    /* Tirei os eventos por que esta flodando sem parar ( tenho que resolver ainda )
     
     client.on("messageUpdate", async (oldMessage, newMessage) => {
       if (oldMessage.content === newMessage.content) {
@@ -222,7 +252,7 @@ client.on("message", async message => {
         .setDescription(`Registro de apelidos alterados por ${newMember}. \n\n **• Informações** \n ▪︎ **Antes**: ${membros[0]} \n ▪︎ **Depois**: ${membros[1]} \n ▪︎ **Servidor**: ${newMember.guild.name}`);
       canal.send(ApelidoEmbed);
       }
-    }) */
+    })
 
 });
 
