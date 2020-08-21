a57e541e8c65b2dc570123d918b03d7edf67a028
Duarte <descionunesd44@gmail.com>
2020-08-21 00:48:09 -0300

    update

--- eventos/guild/messageDelete.js
+++ eventos/guild/messageDelete.js
@@ -1,11 +1,13 @@
 const { MessageEmbed } = require("discord.js");
-const firebase = require('firebase')
-const database = firebase.database()
+/* const firebase = require('firebase')
+const database = firebase.database() */
 
 module.exports = async (message, channel) => {
 
-    let canais = await database.ref(`Servidores/${message.guild.id}/Canais/CanalLog`).once(`value`)
-    canais = canais.val()
+    if (!message || message.channel.type !== 'text' || message.author.bot) return;
+
+    /* let canais = await database.ref(`Servidores/${message.guild.id}/Canais/CanalLog`).once(`value`)
+    canais = canais.val() */
   
     let DeleteEmbed = new MessageEmbed()
       .setAuthor(`Mensagem Deletada`, message.guild.iconURL({dynamic: true}))
@@ -13,7 +15,5 @@ module.exports = async (message, channel) => {
       .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
       .setDescription(`Registro de mensagens deletadas por ${message.author}. \n\n **• Informações** \n ▪︎ **Mensagem Deletada**: ${message} \n ▪︎ **No canal**: ${message.channel} \n ▪︎ **Servidor**: ${message.guild.name}`)
   
-    const canal = message.guild.channels.cache.get(canais);
-      if (!canal) return;
-    return canal.send(DeleteEmbed);
+    return message.channel.send(DeleteEmbed);
 }
