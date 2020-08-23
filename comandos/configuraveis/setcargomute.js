49681ff2cf041f04b07e2d9a7abdfeb96aeb7ef1
Duarte <descionunesd44@gmail.com>
2020-08-22 21:22:38 -0300

    Update setcargomute.js

--- comandos/configuraveis/setcargomute.js
+++ comandos/configuraveis/setcargomute.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "setmute",
     aliases: ["setmute", "setcargomute", "mutecargo"],
     category: "configuraveis",
-    run: async (client, message, database) => {
+    run: async (client, message, args, database) => {
       const { MessageEmbed } = require(`discord.js`);
 
       if (!message.member.hasPermission("ADMINISTRATOR")) {
@@ -27,12 +27,12 @@ module.exports = {
          return;
         }
   
-        database.ref(`/Servidores/${message.guild.id}/Cargos/CargosMute/CargoMute`).set(mention.id)
+        await database.ref(`/Servidores/${message.guild.id}/Cargos/CargosMute/CargoMute`).set(mention.id)
         
-        const embed = new MessageEmbed()
-            .setDescription(`<:certo:736447597102760007> **| SUCESSO AO SETAR O CARGO** \n\n • **Informações:** \n • Cargo de Mute definido para: ${mention}`)
-            .setColor("#2f3136")
-            .setFooter(`Atenciosamente, ${client.user.username}`, client.user.displayAvatarURL())
+      const embed = new MessageEmbed()
+        .setDescription(`<:certo:736447597102760007> **| SUCESSO AO SETAR O CARGO** \n\n • **Informações:** \n • Cargo de Mute definido para: ${mention}`)
+        .setColor("#2f3136")
+        .setFooter(`Atenciosamente, ${client.user.username}`, client.user.displayAvatarURL())
       message.delete().catch();
       message.channel.send(embed);
     },
