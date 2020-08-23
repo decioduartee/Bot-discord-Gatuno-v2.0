f6c9710adc59a7f859e498cc80718cd950fb71a2
Duarte <descionunesd44@gmail.com>
2020-08-22 22:56:20 -0300

    Update perdao.js

--- comandos/moderador/perdao.js
+++ comandos/moderador/perdao.js
@@ -27,10 +27,7 @@ module.exports = {
         }
 
         /* let bannedMemberInfo = await message.guild */
-        let membro = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase())
-        let membroUnban = await client.users.fetch(membro)
-        let ban = await message.guild.fetchBans();
-
+        let membro = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
         let motivo = args.slice(1).join(" ");
 
         const embed = new MessageEmbed()
@@ -144,6 +141,9 @@ module.exports = {
 
           unban.on('collect', async r => {
             msg.delete();
+
+            let membroUnban = await client.users.fetch(membro)
+            let ban = await message.guild.fetchBans();
 
             if (!ban.get(member.id)) {
               const ErroUnban = new MessageEmbed()
