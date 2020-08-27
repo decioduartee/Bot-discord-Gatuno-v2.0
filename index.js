cb06614866f97394754477db4dba4fb8fbe4f844
Duarte <descionunesd44@gmail.com>
2020-08-27 19:31:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const firebase = require("firebase");
+const { MessageEmbed } = require("discord.js")
 
 const client = new Discord.Client({ 
   partials: ["MESSAGE", "CHANNEL", "REACTION"] }, 
