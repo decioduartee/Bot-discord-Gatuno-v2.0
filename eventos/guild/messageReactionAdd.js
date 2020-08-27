8c8da81c3ca2132c7b03092e654bccd1ace975b4
Duarte <descionunesd44@gmail.com>
2020-08-27 18:13:32 -0300

    update

---
+++ eventos/guild/messageReactionAdd.js
@@ -0,0 +1,12 @@
+module.exports = async (member, message) => {
+
+let message = reaction.message, emoji = reaction.emoji;
+  
+  if(user.bot) return;
+
+  if (emoji.name == '📩') {
+    message.guild.members.fetch(user.id).then(x => {
+        reaction.users.remove(user)
+    });
+  }
+}
